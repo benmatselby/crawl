843c627cfd58cb28c679680f17d83de3e054c9e5
Ben Selby <benmatselby@gmail.com>
2019-07-03 09:53:30 +0100

    Start padding out the test infrastructure for Crawl Page
    May need refactoring when we start with channels

--- main_test.go
+++ main_test.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"regexp"
 	"testing"
 )
 
@@ -27,6 +32,90 @@ func TestRunCanParseFlags(t *testing.T) {
 				if err.Error() != tc.expected.Error() {
 					t.Fatalf("expected %v, got %v", tc.expected, err)
 				}
+			}
+		})
+	}
+}
+
+func TestCrawlPage(t *testing.T) {
+	tt := []struct {
+		name     string
+		URL      string
+		response string
+		goodURLs []string
+		badURLs  []string
+		expected []string
+	}{
+		{
+			name: "a single URL",
+			URL:  "/single-url",
+			response: `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<meta http-equiv="X-UA-Compatible" content="ie=edge">
+				<title>Document</title>
+			</head>
+			<body>
+			<p>A paragraph</p>
+			<a href="http://GOOD_URL/blog">a link</a>
+			</body>
+			</html>`,
+			expected: []string{"http://GOOD_URL/blog"},
+		},
+		{
+			name: "a single good URL with other urls",
+			URL:  "/single-url-with-other-urls",
+			response: `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<meta http-equiv="X-UA-Compatible" content="ie=edge">
+				<title>Document</title>
+			</head>
+			<body>
+			<p>A paragraph</p>
+			<a href="http://GOOD_URL/blog">a link</a>
+			<a href="https://twitter.com/">a link</a>
+			<a href="https://github.com/">a link</a>
+			</body>
+			</html>`,
+			expected: []string{"http://GOOD_URL/blog"},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			re := regexp.MustCompile(`GOOD_URL`)
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintln(w, re.ReplaceAllString(tc.response, r.Host))
+			}))
+			defer ts.Close()
+
+			URL, err := url.ParseRequestURI(ts.URL + tc.URL)
+			if err != nil {
+				t.Fatalf("was not expected error, got '%s'", err)
+			}
+
+			urls, err := CrawlPage(URL)
+			if err != nil {
+				t.Fatalf("was not expected error, got '%s'", err)
+			}
+
+			testServerURL, err := url.ParseRequestURI(ts.URL)
+			if err != nil {
+				t.Fatalf("was not expected error, got '%s'", err)
+			}
+
+			var expected []string
+			for _, url := range tc.expected {
+				expected = append(expected, re.ReplaceAllString(url, testServerURL.Host))
+			}
+
+			if fmt.Sprintf("%s", urls) != fmt.Sprintf("%s", expected) {
+				t.Fatalf("expected '%s', got '%s'", tc.expected, urls)
 			}
 		})
 	}
